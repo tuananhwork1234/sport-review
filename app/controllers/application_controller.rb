2ad244a93905bfee6a16c949d990fa2f98f33bf2
chinguyen <chi.ny184049@sis.hust.edu.vn>
2021-12-13 09:01:07 +0000

    Add user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,9 +1,12 @@
 class ApplicationController < ActionController::Base
+    protect_from_forgery with: :exception
+
     before_action :configure_permitted_parameters, if: :devise_controller?
-    
+
     protected
-        def configure_permitted_parameters
-            devise_parameter_sanitizer.permit :sign_up, keys: [:avatar, :last_name, :first_name, :age, :favortie_sport]
-            devise_parameter_sanitizer.permit :account_update, keys: [:avatar, :last_name, :first_name, :age, :favortie_sport]
-        end
+
+    def configure_permitted_parameters
+        devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:avatar,:first_name, :last_name, :favorite_sport, :age, :email, :password)}
+        devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:avatar,:first_name, :last_name, :favorite_sport, :age, :email, :password, :current_password)}
+    end
 end
