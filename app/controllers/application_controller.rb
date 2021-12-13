435ab9572bb871e00ad6618a2c8bdef71bb41bd7
chinguyen <chi.ny184049@sis.hust.edu.vn>
2021-12-13 08:14:36 +0000

    Add User

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,7 +3,7 @@ class ApplicationController < ActionController::Base
     
     protected
         def configure_permitted_parameters
-            devise_parameter_sanitizer.permit :sign_up, keys: [:last_name, :first_name, :age, :favortie_sport]
-            devise_parameter_sanitizer.permit :account_update, keys: [:last_name, :first_name, :age, :favortie_sport]
+            devise_parameter_sanitizer.permit :sign_up, keys: [:avatar, :last_name, :first_name, :age, :favortie_sport]
+            devise_parameter_sanitizer.permit :account_update, keys: [:avatar, :last_name, :first_name, :age, :favortie_sport]
         end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,12 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
+  has_one_attached :avatar
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+         
+    validates_uniqueness_of :first_name
          
     validates :last_name, presence: true
     validates :first_name, presence: true
